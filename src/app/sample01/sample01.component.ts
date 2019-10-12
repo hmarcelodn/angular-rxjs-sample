d80a709c946b0c6ee1f4c8a34d4fdf1b3bc65dab
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2019-10-12 02:44:24 -0300

    Streamming Sample Sprites

--- src/app/sample01/sample01.component.ts
+++ src/app/sample01/sample01.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { Observable, BehaviorSubject, Subject, interval, of, fromEvent } from 'rxjs';
+import { scan, takeUntil, tap, withLatestFrom, map, takeWhile } from 'rxjs/operators';
 import * as converter from 'number-to-words';
 
 @Component({
@@ -9,27 +10,70 @@ import * as converter from 'number-to-words';
 })
 export class Sample01Component implements OnInit, OnDestroy {
 
-  progress$: Observable<number>;
-  words$ = new BehaviorSubject('');
+  interval$: Observable<number>;
+  isNormal$ = new Subject<boolean>();
+  isNormalStanding$ = new Subject<boolean>();
+  isSsj$ = new Subject<boolean>();
+  isSsjStanding$ = new Subject<boolean>();
+  words$ = new Subject();
+  progress$ = new BehaviorSubject<number>(0);
   componentDestroyed$ = new Subject();
+  increasing$ = new Subject();
 
   constructor() { }
 
   ngOnInit() {
-    this.progress$ = new Observable((observer: any) => {
-      let times = 0;
 
-      setInterval(() => {
-        if (times > 100) {
-          observer.complete();
-        }
+    const begin = fromEvent(document, 'keydown');
+    const end = fromEvent(document, 'keyup');
 
-        observer.next(times++);
-      }, 100);
+    this.progress$.next(0);
+    this.words$.next('');
+    this.increasing$.next(false);
+
+    begin.pipe(takeUntil(this.componentDestroyed$)).subscribe(() => {
+      this.increasing$.next(true);
+      if (this.progress$.getValue()  <= 98) {
+        this.progress$.next(this.progress$.getValue() + 2);
+      }
     });
 
-    this.progress$.subscribe(
-      (progress: number) => this.words$.next(converter.toWords(progress)),
+    end.pipe(takeUntil(this.componentDestroyed$)).subscribe(() => {
+      this.increasing$.next(false);
+      this.interval$ = interval(100);
+      this.interval$.pipe(takeUntil(begin)).subscribe(() => {
+        if (this.progress$.getValue() >= 2) {
+          this.progress$.next(this.progress$.getValue() - 2);
+        }
+      });
+    });
+
+    this.progress$.pipe(takeUntil(this.componentDestroyed$)).subscribe(
+      (progress: number) => {
+        console.log({ level: progress });
+        this.words$.next(converter.toWords(progress));
+        if (progress >= 1 && progress < 20) {
+          this.isNormalStanding$.next(true);
+          this.isNormal$.next(false);
+          this.isSsj$ .next(false);
+          this.isSsjStanding$.next(false);
+        } else if (progress > 20 && progress < 60) {
+          this.isNormalStanding$.next(false);
+          this.isNormal$.next(true);
+          this.isSsj$ .next(false);
+          this.isSsjStanding$.next(false);
+        } else if (progress > 60 && progress < 90) {
+          this.isNormalStanding$.next(false);
+          this.isNormal$.next(false);
+          this.isSsj$ .next(true);
+          this.isSsjStanding$.next(false);
+        } else if(progress > 90) {
+          this.isNormalStanding$.next(false);
+          this.isNormal$.next(false);
+          this.isSsj$ .next(false);
+          this.isSsjStanding$.next(true);
+        }
+      },
       (error: any) => console.log('Handling Error'),
       () => console.log('Stream completed')
     );
