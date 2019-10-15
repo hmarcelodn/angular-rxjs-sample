87473454836ad52cb246452783d8dfdcf22f9bb3
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2019-10-14 22:25:12 -0300

    Operators

--- src/app/sample01/sample01.component.ts
+++ src/app/sample01/sample01.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, BehaviorSubject, Subject, interval, fromEvent } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, scan } from 'rxjs/operators';
 import * as converter from 'number-to-words';
 
 @Component({
@@ -20,6 +20,7 @@ export class Sample01Component implements OnInit, OnDestroy {
   componentDestroyed$ = new Subject();
   increasing$ = new Subject();
   itsOver$ = new Subject<boolean>();
+  level$ =  new Subject<number>();
 
   constructor() { }
 
@@ -37,7 +38,7 @@ export class Sample01Component implements OnInit, OnDestroy {
     begin.pipe(takeUntil(this.componentDestroyed$)).subscribe(() => {
       this.increasing$.next(true);
       if (this.progress$.getValue()  <= 98) {
-        this.progress$.next(this.progress$.getValue() + 2);
+        this.level$.next(2);
       }
     });
 
@@ -47,45 +48,47 @@ export class Sample01Component implements OnInit, OnDestroy {
       this.interval$ = interval(100);
       this.interval$.pipe(takeUntil(begin)).subscribe(() => {
         if (this.progress$.getValue() >= 2) {
-          this.progress$.next(this.progress$.getValue() - 2);
+          this.level$.next(-2);
         }
       });
     });
 
     // Observer 3
-    this.progress$.pipe(takeUntil(this.componentDestroyed$)).subscribe(
-      (progress: number) => {
-        console.log({ level: progress });
-        this.words$.next(converter.toWords(progress));
-        if (progress >= 1 && progress < 20) {
-          this.isNormalStanding$.next(true);
-          this.isNormal$.next(false);
-          this.isSsj$ .next(false);
-          this.isSsjStanding$.next(false);
-          this.itsOver$.next(false);
-        } else if (progress > 20 && progress < 60) {
-          this.isNormalStanding$.next(false);
-          this.isNormal$.next(true);
-          this.isSsj$ .next(false);
-          this.isSsjStanding$.next(false);
-          this.itsOver$.next(false);
-        } else if (progress > 60 && progress < 90) {
-          this.isNormalStanding$.next(false);
-          this.isNormal$.next(false);
-          this.isSsj$ .next(true);
-          this.isSsjStanding$.next(false);
-          this.itsOver$.next(false);
-        } else if(progress > 90) {
-          this.isNormalStanding$.next(false);
-          this.isNormal$.next(false);
-          this.isSsj$ .next(false);
-          this.isSsjStanding$.next(true);
-          this.itsOver$.next(true);
-        }
-      },
-      (error: any) => console.log('Handling Error'),
-      () => console.log('Stream completed')
-    );
+    const source = this.level$.pipe(scan((acc, curr) => acc + curr));
+    source.pipe(takeUntil(this.componentDestroyed$)).subscribe(progress => {
+      console.log({ level: progress });
+      this.progress$.next(progress);
+      this.words$.next(converter.toWords(progress));
+      if (progress >= 0 && progress < 20) {
+        this.isNormalStanding$.next(true);
+        this.isNormal$.next(false);
+        this.isSsj$ .next(false);
+        this.isSsjStanding$.next(false);
+        this.itsOver$.next(false);
+      } else if (progress > 20 && progress < 60) {
+        this.isNormalStanding$.next(false);
+        this.isNormal$.next(true);
+        this.isSsj$ .next(false);
+        this.isSsjStanding$.next(false);
+        this.itsOver$.next(false);
+      } else if (progress > 60 && progress < 90) {
+        this.isNormalStanding$.next(false);
+        this.isNormal$.next(false);
+        this.isSsj$ .next(true);
+        this.isSsjStanding$.next(false);
+        this.itsOver$.next(false);
+      } else if (progress > 90) {
+        this.isNormalStanding$.next(false);
+        this.isNormal$.next(false);
+        this.isSsj$ .next(false);
+        this.isSsjStanding$.next(true);
+        this.itsOver$.next(true);
+      }
+    });
+
+    setTimeout(() => {
+      this.level$.next(0);
+    }, 100);
   }
 
   ngOnDestroy(): void {
